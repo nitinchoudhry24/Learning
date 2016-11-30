baef1016a430f5605b986c8d4e532b407883490c
nitinchoudhry24 <nitinchoudhry@icloud.com>
2016-11-30 13:28:06 +0530

    JavaProjects

---
+++ JavaProjects/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/***
+ * 
+ * @author nitinchoudhry
+ * First Java Project
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String Args[])
+	{
+		System.out.println("Hello World");
+		
+	}
+
+}
